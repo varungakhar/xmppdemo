4bb028c776d845359da1ae97c9c7e5fb13be2bf1
KindleBit <kindlebit@varun-mac-mini.local>
2016-04-15 17:29:01 +0530

    Initial Commit

---
+++ XMPPDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPDemo
+//
+//  Created by KindleBit on 15/04/16.
+//  Copyright © 2016 KindleBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPDemo
+//
+//  Created by KindleBit on 15/04/16.
+//  Copyright © 2016 KindleBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
